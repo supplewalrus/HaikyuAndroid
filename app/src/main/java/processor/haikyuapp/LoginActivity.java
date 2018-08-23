02381e69505ac64899bb051b036f73dac0cfa0fc
Brandon Patrick <bapatrick@verisk.com>
2018-08-23 11:48:41 -0600

    Facebook login working much better

--- app/src/main/java/processor/haikyuapp/LoginActivity.java
+++ app/src/main/java/processor/haikyuapp/LoginActivity.java
@@ -1,107 +1,4 @@
 package processor.haikyuapp;
-//
-//import android.content.Intent;
-//import android.support.annotation.NonNull;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.util.Log;
-//import android.view.View;
-//import android.widget.Toast;
-//
-//import com.firebase.ui.auth.AuthUI;
-//import com.firebase.ui.auth.IdpResponse;
-//import com.google.android.gms.tasks.OnCompleteListener;
-//import com.google.android.gms.tasks.Task;
-//import com.google.firebase.auth.AuthResult;
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.auth.FirebaseUser;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class LoginActivity extends AppCompatActivity
-//{
-//    private static final int RC_SIGN_IN = 123;
-//    private FirebaseAuth auth;
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState)
-//    {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_login);
-//        auth = FirebaseAuth.getInstance();
-//        if(auth.getCurrentUser() != null){
-//            //if already logged in. might not need this
-//            Log.d("AUTH", auth.getCurrentUser().getEmail());
-//        } else {
-//            List<AuthUI.IdpConfig> providers = Arrays.asList(
-//                    new AuthUI.IdpConfig.EmailBuilder().build(),
-//                    new AuthUI.IdpConfig.FacebookBuilder().build());
-//
-//            // Create and launch sign-in intent. Uncomment when we have logo and theme
-//            // also if we get privacy policy and terms of service
-//            startActivityForResult(
-//                AuthUI.getInstance()
-//                    .createSignInIntentBuilder()
-//                    .setAvailableProviders(providers)
-//                    //.setLogo(R.drawable.my_great_logo)      // Set logo drawable
-//                    //.setTheme(R.style.MySuperAppTheme)      // Set theme
-//                    //.setTosUrl("https://superapp.example.com/terms-of-service.html")
-//                    //.setPrivacyPolicyUrl("https://superapp.example.com/privacy-policy.html")
-//                    .build(),
-//                RC_SIGN_IN);
-//        }
-////        findViewById(R.id.log_out_button).setOnClickListener(this);
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-//    {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == RC_SIGN_IN) {
-//            IdpResponse response = IdpResponse.fromResultIntent(data);
-//
-//            if (resultCode == RESULT_OK) {
-//                // Successfully signed in
-//                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//                Log.d("AUTH", auth.getCurrentUser().getEmail());
-//
-//            } else {
-//                // Sign in failed. If response is null the user canceled the
-//                // sign-in flow using the back button. Otherwise check
-//                // response.getError().getErrorCode() and handle the error.
-//                // ...
-//                Log.d("AUTH", "NOT AUTHENTICATED");
-//            }
-//        }
-//    }
-//
-//
-//    //Move this to other activities.
-////    @Override
-////    public void onClick(View view)
-////    {
-////        if(view.getId() == R.id.log_out_button){
-////            AuthUI.getInstance()
-////                    .signOut(this)
-////                    .addOnCompleteListener(new OnCompleteListener<Void>()
-////                    {
-////                        @Override
-////                        public void onComplete(@NonNull Task<Void> task)
-////                        {
-////                            Log.d("AUTH", "USER LOGGED OUT!");
-////                            finish();
-////                        }
-////                    });
-////
-////        }
-////    }
-//}
-
-
-
 
 /*
  * Copyright 2016 Google Inc. All Rights Reserved.
@@ -122,14 +19,11 @@ package processor.haikyuapp;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
-import android.support.annotation.StyleRes;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -142,7 +36,6 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.AuthUI.IdpConfig;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.common.Scopes;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -160,9 +53,7 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
 
     private static final String GOOGLE_TOS_URL = "https://www.google.com/policies/terms/";
-    private static final String FIREBASE_TOS_URL = "https://firebase.google.com/terms/";
     private static final String GOOGLE_PRIVACY_POLICY_URL = "https://www.google.com/policies/privacy/";
-    private static final String FIREBASE_PRIVACY_POLICY_URL = "https://firebase.google.com/terms/analytics/#7_privacy";
 
     private static final int RC_SIGN_IN = 100;
 
@@ -211,20 +102,18 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
-//        if (Utils.isGoogleMisconfigured(this)) {
-//            mUseGoogleProvider.setChecked(false);
-//            mUseGoogleProvider.setEnabled(false);
-//            mUseGoogleProvider.setText(R.string.google_label_missing_config);
-//            setGoogleScopesEnabled(false);
-//        } else {
-//            setGoogleScopesEnabled(mUseGoogleProvider.isChecked());
-//            mUseGoogleProvider.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-//                @Override
-//                public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-//                    setGoogleScopesEnabled(checked);
-//                }
-//            });
-//        }
+
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setTheme(R.style.GreenTheme)
+                        .setLogo(R.drawable.ic_googleg_color_144dp)
+                        .setAvailableProviders(getSelectedProviders())
+                        .setTosAndPrivacyPolicyUrls(GOOGLE_TOS_URL,
+                                GOOGLE_PRIVACY_POLICY_URL)
+                        .setIsSmartLockEnabled(mEnableCredentialSelector.isChecked(),
+                                mEnableHintSelector.isChecked())
+                        .build(),
+                RC_SIGN_IN);
 
         if (Utils.isFacebookMisconfigured(this)) {
             mUseFacebookProvider.setChecked(false);
@@ -245,21 +134,19 @@ public class LoginActivity extends AppCompatActivity {
                 || Utils.isFacebookMisconfigured(this)) {
             showSnackbar(R.string.configuration_required);
         }
-
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
-            mDarkTheme.setChecked(true);
-        }
     }
+
+    //DO THIS FROM THE START
 
     @OnClick(R.id.sign_in)
     public void signIn(View view) {
         startActivityForResult(
                 AuthUI.getInstance().createSignInIntentBuilder()
-                        .setTheme(getSelectedTheme())
-                        .setLogo(getSelectedLogo())
+                        .setTheme(R.style.GreenTheme)
+                        .setLogo(R.drawable.ic_googleg_color_144dp)
                         .setAvailableProviders(getSelectedProviders())
-                        .setTosAndPrivacyPolicyUrls(getSelectedTosUrl(),
-                                getSelectedPrivacyPolicyUrl())
+                        .setTosAndPrivacyPolicyUrls(GOOGLE_TOS_URL,
+                                GOOGLE_PRIVACY_POLICY_URL)
                         .setIsSmartLockEnabled(mEnableCredentialSelector.isChecked(),
                                 mEnableHintSelector.isChecked())
                         .build(),
@@ -328,55 +215,12 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(MainActivity.createIntent(this, response));
     }
 
-    @OnClick({R.id.default_theme, R.id.purple_theme, R.id.green_theme, R.id.dark_theme})
-    public void toggleDarkTheme() {
-        int mode = mDarkTheme.isChecked() ?
-                AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_AUTO;
-        AppCompatDelegate.setDefaultNightMode(mode);
-        getDelegate().setLocalNightMode(mode);
-    }
-
-    @StyleRes
-    private int getSelectedTheme() {
-        if (mGreenTheme.isChecked()) {
-            return R.style.GreenTheme;
-        }
-
-        if (mPurpleTheme.isChecked()) {
-            return R.style.PurpleTheme;
-        }
-
-        return AuthUI.getDefaultTheme();
-    }
-
-    @DrawableRes
-    private int getSelectedLogo() {
-        if (mFirebaseLogo.isChecked()) {
-            return R.drawable.firebase_auth_120dp;
-        } else if (mGoogleLogo.isChecked()) {
-            return R.drawable.ic_googleg_color_144dp;
-        }
-        return AuthUI.NO_LOGO;
-    }
-
     private List<IdpConfig> getSelectedProviders() {
         List<IdpConfig> selectedProviders = new ArrayList<>();
-
-        if (mUseGoogleProvider.isChecked()) {
-            selectedProviders.add(
-                    new IdpConfig.GoogleBuilder().setScopes(getGoogleScopes()).build());
-        }
 
         if (mUseFacebookProvider.isChecked()) {
             selectedProviders.add(new IdpConfig.FacebookBuilder()
                     .setPermissions(getFacebookPermissions())
-                    .build());
-        }
-
-        if (mUseEmailProvider.isChecked()) {
-            selectedProviders.add(new IdpConfig.EmailBuilder()
-                    .setRequireName(mRequireName.isChecked())
-                    .setAllowNewAccounts(mAllowNewEmailAccounts.isChecked())
                     .build());
         }
 
@@ -387,43 +231,10 @@ public class LoginActivity extends AppCompatActivity {
         return selectedProviders;
     }
 
-    private String getSelectedTosUrl() {
-        if (mUseGoogleTos.isChecked()) {
-            return GOOGLE_TOS_URL;
-        }
-
-        return FIREBASE_TOS_URL;
-    }
-
-    private String getSelectedPrivacyPolicyUrl() {
-        if (mUseGooglePrivacyPolicy.isChecked()) {
-            return GOOGLE_PRIVACY_POLICY_URL;
-        }
-
-        return FIREBASE_PRIVACY_POLICY_URL;
-    }
-
-    private void setGoogleScopesEnabled(boolean enabled) {
-        mGoogleScopesHeader.setEnabled(enabled);
-        mGoogleScopeDriveFile.setEnabled(enabled);
-        mGoogleScopeYoutubeData.setEnabled(enabled);
-    }
-
     private void setFacebookPermissionsEnabled(boolean enabled) {
         mFacebookPermissionsHeader.setEnabled(enabled);
         mFacebookPermissionFriends.setEnabled(enabled);
         mFacebookPermissionPhotos.setEnabled(enabled);
-    }
-
-    private List<String> getGoogleScopes() {
-        List<String> result = new ArrayList<>();
-        if (mGoogleScopeYoutubeData.isChecked()) {
-            result.add("https://www.googleapis.com/auth/youtube.readonly");
-        }
-        if (mGoogleScopeDriveFile.isChecked()) {
-            result.add(Scopes.DRIVE_FILE);
-        }
-        return result;
     }
 
     private List<String> getFacebookPermissions() {
